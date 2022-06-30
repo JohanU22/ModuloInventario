390104c7353338726f5935e0c13de4d9fa60b382
Johan Urbina <johanurborlando2217@gmail.com>
2022-06-30 13:59:29 -0600

    Se agregan las rutas de productos para modificar y eliminar

--- src/rutas/rutasProductos.js
+++ src/rutas/rutasProductos.js
@@ -1,5 +1,5 @@
 const { Router } = require('express');
-const { body } = require('express-validator');
+const { body, query } = require('express-validator');
 const controladorProductos = require('../controladores/controladorProductos');
 const rutas = Router();
 rutas.get('/listar', controladorProductos.Listar);
@@ -11,9 +11,17 @@ body ('TipoProducto')
 .notEmpty().withMessage('No se aceptan valores vacios para el tipo producto').isInt().withMessage('El id del tipoproducto debe ser un entero'),
 controladorProductos.Guardar);
 
+rutas.put('/modificar',
+query('Codigo')
+.notEmpty().withMessage('No se aceptan valores vacios para el codigo').isLength({min:4}).withMessage('La cantidad minima de caracteres son 4 para el codigo de producto'),
+body('Nombre')
+.notEmpty().withMessage('No se aceptan valores vacios para el nombre del producto'),
+controladorProductos.Modificar);
 
-
-
+rutas.delete('/eliminar',
+query('Codigo')
+.notEmpty().withMessage('No se aceptan valores vacios para el codigo'),
+controladorProductos.Eliminar);
 //rutas.put('/modificar',controladorProductos.modificar);
 
 module.exports=rutas;
