fbe758fc1b871a06aed476d3d7c608394f121f1f
JoseAlmendarez19 <josm_almendarezf@unicah.edu>
2022-07-01 13:40:14 -0600

    Se agregó git ignore

---
+++ src/configuraciones/db.js
@@ -0,0 +1,12 @@
+const sequelize = require('sequelize');
+const db = new sequelize(
+    'sifcon', //base
+    'root',  //user
+    'Root0417',  //contraseña
+    {
+        host: 'localhost', //directorio
+        dialect: 'mysql', //motor sql
+        port: 3306, //puerto que utiliza mysql
+    }
+);
+module.exports = db;
