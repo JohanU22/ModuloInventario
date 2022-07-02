3fbe4973ec5c16e35845890a531aa499f1db50f9
JoseFigueroa18 <85241703+JoseFigueroa18@users.noreply.github.com>
2022-07-01 18:19:43 -0600

    Listar en productosproveedores ya es funcional

--- src/rutas/rutasProductosProveedores.js
+++ src/rutas/rutasProductosProveedores.js
@@ -2,9 +2,7 @@ const { Router } = require('express');
 const { body,query } = require('express-validator');
 const controladorProductosProveedores =require('../controladores/controladorProductosProveedores');
 const rutas = Router();
-rutas.get('/listar', controladorProductosProveedores.Listar);
 
-//rutas.post('/guardar',controladorProductos.guardar);
-//rutas.put('/modificar',controladorProductos.modificar);
-
+rutas.get('/listar', 
+controladorProductosProveedores.Listar);
 module.exports=rutas;
