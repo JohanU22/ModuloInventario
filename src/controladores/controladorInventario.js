a48af4225f39e9ae2a324710cc25efe6fd9b45d6
JoseAlmendarez19 <josemariaalmendares@gmail.com>
2022-07-02 12:21:36 -0600

    Listar funcional en la tabla Inventario

---
+++ src/controladores/controladorInventario.js
@@ -0,0 +1,140 @@
+const { validationResult } = require('express-validator');
+const modeloInventario = require('../modelos/modeloInventario');
+const { Op } = require('sequelize');
+
+//Listar
+exports.Listar = async (req, res) => {
+    var msj = {
+        mensaje: ''
+    }
+    try {
+        
+        const Lista = await modeloInventario.findAll();
+        console.log(Lista);
+        res.json(Lista);
+    } catch (error) {
+        console.error(error);
+        res.json(error);
+    }
+};
+
+//Guardar
+exports.Guardar = async (req, res) => {
+    const validaciones = validationResult(req);
+    console.log(validaciones.errors);
+
+    const msj = {
+        mensaje: ''
+    };
+    if (validaciones.errors.length > 0) {
+        validaciones.errors.forEach(element => {
+            msj.mensaje += element.msg + '. ';
+        });
+
+    } else {
+        const {  fechahora, faltante, sobrante, usuarios_idregistro, estaciones_NumeroEstacion} = req.body;
+
+        try {
+            await modeloInventario.create(
+                {
+                    fechahora:fechahora,
+                    faltante:faltante,
+                    sobrante:sobrante,
+                    usuarios_idregistro: usuarios_idregistro,
+                    estaciones_NumeroEstacion:estaciones_NumeroEstacion
+                });
+            msj.mensaje = 'Registro almacenado';
+
+        } catch (error) {
+            msj.mensaje = 'Error al guardar los datos';
+
+        }
+
+
+    }
+    res.json(msj);
+};
+
+//Editar
+exports.Editar = async(req, res) => {
+    const validaciones = validationResult(req);
+    console.log(validaciones.errors);
+    const msj = {
+        mensaje: ''
+    };
+    if (validaciones.errors.length > 0) {
+        validaciones.errors.forEach(element => {
+            msj.mensaje += element.msg + '. ';
+        });
+
+    } else {
+        const { id } = req.query;
+        const { fechahora, faltante, sobrante, usuarios_idregistro, estaciones_NumeroEstacion } = req.body;
+
+        try {
+            var buscarInventario = await modeloInventario.findOne({
+                where:{
+                    id:id
+                }
+            });
+           
+            if (!buscarInventario) {
+                msj.mensaje = 'El id del inventario no existe'
+                
+            }else{
+                buscarInventario.fechahora = fechahora;
+                buscarInventario.faltante = faltante;
+                buscarInventario.sobrante = sobrante ;
+                buscarInventario.usuarios_idregistro = usuarios_idregistro ;
+                buscarInventario.estaciones_NumeroEstacion = estaciones_NumeroEstacion;
+                await buscarInventario.save(); 
+                msj.mensaje = 'Registros actualizado';
+            }
+        } catch (error) {
+            msj.mensaje = 'Error al editar los datos';
+
+        }
+    }
+    res.json(msj);
+};
+
+//Eliminar
+exports.Eliminar = async(req, res)=> {
+    const validaciones = validationResult(req);
+    console.log(validaciones.errors);
+    const msj = {
+        mensaje: ''
+    };
+    if (validaciones.errors.length > 0) {
+        validaciones.errors.forEach(element => {
+            msj.mensaje += element.msg + '. ';
+        });
+
+    } else {
+        const { id } = req.query;
+        try {
+            var buscarInventario = await modeloInventario.findOne({
+                where:{
+                    id:id
+                }
+            });
+           
+            if (!buscarInventario) {
+                msj.mensaje = 'El id del inventario no existe'
+                
+            }else{                
+                await modeloInventario.destroy({
+                    where: 
+                    {
+                        id:id
+                    }
+                });
+                msj.mensaje = 'Registros  Eliminados';
+            }
+        } catch (error) {
+            msj.mensaje = 'Error al eliminar los datos';
+
+        }
+    }
+    res.json(msj);
+ };
---
+++ src/rutas/rutasInventario.js
@@ -0,0 +1,30 @@
+const {Router} = require('express');
+const { body, query } = require('express-validator');
+const controladorInventario = require('../controladores/controladorInventario');
+const rutas = Router();
+//Listar
+rutas.get('/listar', controladorInventario.Listar);
+
+//Guardar
+rutas.post('/guardar',
+body('usuarios_idregistro')
+.notEmpty().withMessage('No se aceptan valores vacios para el id de registro').isInt().withMessage('El id de registro debe ser entero'),
+body('estaciones_NumeroEstacion')
+.notEmpty().withMessage('No se aceptan valores vacios para el id de estacion').isInt().withMessage('El id de estacion debe ser entero'),
+controladorInventario.Guardar);
+
+//Editar
+rutas.put('/editar',
+body('usuarios_idregistro')
+.notEmpty().withMessage('No se aceptan valores vacios para el id de registro').isInt().withMessage('El id de registro debe ser entero'),
+body('estaciones_NumeroEstacion')
+.notEmpty().withMessage('No se aceptan valores vacios para el id de estacion').isInt().withMessage('El id de estacion debe ser entero'),
+controladorInventario.Editar);
+
+//Elimnar
+rutas.delete('/eliminar',
+query('id')
+.notEmpty().withMessage('No se aceptan valores vacios para el id de inventario').isInt().withMessage('El id de inventario debe ser entero'),
+controladorInventario.Eliminar);
+
+module.exports = rutas;
