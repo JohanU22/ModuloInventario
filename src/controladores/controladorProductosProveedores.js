6f1768ce684fece1e8b476e29304c93c458a1749
JoseFigueroa18 <85241703+JoseFigueroa18@users.noreply.github.com>
2022-07-01 18:32:24 -0600

    Guardar en productosproveedores ya es funcional

--- src/controladores/controladorProductosProveedores.js
+++ src/controladores/controladorProductosProveedores.js
@@ -13,3 +13,30 @@ exports.Listar = async (req, res) => {
         res.json(error);
     }
 };
+
+exports.Guardar = async (req, res) => {
+    const validaciones = validationResult(req);
+    console.log(validaciones.errors);
+    const msj = { 
+        mensaje: ''
+    };
+    if(validaciones.errors.length > 0){
+        validaciones.errors.forEach(element => {
+            msj.mensaje += element.msg + '. ';
+        });
+    }
+    else{
+        const { idproducto, idproveedor } = req.body;
+        try {
+            await modeloProductoProveedores.create({
+                idproducto: idproducto, 
+                idproveedor: idproveedor
+            });
+            msj.mensaje='Registro almacenado';
+        } 
+        catch (error) {
+            msj.mensaje='No existe el id';
+        }
+    }
+    res.json(msj);    
+};
--- src/rutas/rutasProductosProveedores.js
+++ src/rutas/rutasProductosProveedores.js
@@ -6,3 +6,14 @@ const rutas = Router();
 rutas.get('/listar', 
 controladorProductosProveedores.Listar);
 module.exports=rutas;
+
+rutas.post('/guardar', 
+body ('idproducto')
+    .notEmpty().withMessage('No se aceptan valores vacios para el idproveedor')
+    .isString().withMessage('El codigo de productos debe ser un string')
+    .isLength({max:15}).withMessage('La cantidad maxima de caracteres son 15 para el codigo de productos')
+    , 
+body ('idproveedor')
+    .notEmpty().withMessage('No se aceptan valores vacios para el idproveedor')
+    .isInt().withMessage('El codigo de proveedores debe ser entero'), 
+controladorProductosProveedores.Guardar);
