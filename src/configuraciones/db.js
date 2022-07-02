a23e32c477a3894e3c24b6627a627f9f91eed395
JoseAlmendarez19 <josemariaalmendares@gmail.com>
2022-07-02 12:23:33 -0600

    Guardar funcional de la tabla Inventario

--- src/configuraciones/db.js
+++ src/configuraciones/db.js
@@ -2,7 +2,7 @@ const sequelize = require('sequelize');
 const db = new sequelize(
     'sifcon', //base
     'root',  //user
-    'Root0417',  //contraseña
+    'JMAF2002',  //contraseña
     {
         host: 'localhost', //directorio
         dialect: 'mysql', //motor sql
